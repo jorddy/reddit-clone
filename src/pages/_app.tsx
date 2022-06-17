ecdd0170ec690c1fc6f69975ef80344c2fb8fa8a
Jake <jake.ord345@gmail.com>
2022-06-17 16:20:06 +0100

    Wrapped react query client in state and hydration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,31 +2,34 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Head from "next/head";
 import Header from "@/components/header";
-
-const queryClient = new QueryClient();
+import { useState } from "react";
 
 const MyApp = ({
   Component,
-  pageProps: { session, ...pageProps }
+  pageProps: { session, dehydratedState, ...pageProps }
 }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
-        <Head>
-          <title>Reddit 2.0</title>
-        </Head>
+        <Hydrate state={dehydratedState}>
+          <Head>
+            <title>Reddit 2.0</title>
+          </Head>
 
-        <div className='h-screen overflow-y-scroll bg-slate-200'>
-          <Header />
-          <Component {...pageProps} />
-        </div>
+          <div className='h-screen overflow-y-scroll bg-slate-200'>
+            <Header />
+            <Component {...pageProps} />
+          </div>
 
-        <Toaster position='bottom-center' />
-        <ReactQueryDevtools />
+          <Toaster position='bottom-center' />
+          <ReactQueryDevtools />
+        </Hydrate>
       </QueryClientProvider>
     </SessionProvider>
   );
